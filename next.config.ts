f8c049f866083d2e7f569901350d07da325b4418
barto <bartlomiej.calko@gmail.com>
2025-05-05 17:54:54 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -7,22 +7,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Ensure these Node.js modules are not included in client-side bundles
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        child_process: false,
-        fs: false,
-        os: false,
-        'gcp-metadata': false,
-        net: false,
-        tls: false,
-        dns: false
-      };
-    }
-    return config;
-  }
+  
 };
 
 export default nextConfig;
